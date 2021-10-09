70377774125ec4f1aaa76f7e88be6f0c938260ef
tzt <742578852@qq.com>
2021-10-10 01:24:19 +0800

    统计页面

--- pages/tongji/tongji.vue
+++ pages/tongji/tongji.vue
@@ -1,323 +1,247 @@
 <template>
 	<view>
-		<view>
-			<view style="text-align: center;font-size: 40rpx;margin-top: 25rpx;">隐患整改率统计</view>
+		<!-- 安全运行天数 -->
+		<view style="background-image: url(../../static/bg.png);height: 400rpx;">
+			<view style="text-align: center;padding-top:120rpx ;color: #FFFFFF;font-size: 80rpx;">{{day}}</view>
+			<view style="text-align: center;margin-top: 5rpx;color: #FFFFFF;font-size: 40rpx;">安全运行天数</view>
+		</view>
+
+		<!--隐患统计  -->
+		<view style="margin-top: 40rpx;">
+			<text style="font-size: 40rpx;font-weight: 500;margin: 20rpx;">隐患统计</text>
+			<view style="margin-top: 20rpx;">
+				<u-line color="green" border-style="dashed"></u-line>
+			</view>
+			<view style="text-align: center;margin:20rpx ;font-size: 30rpx;">隐患整改率统计</view>
 			<view>
 				<view class="cu-form-group">
 					<view class="title"><span style="color:red;"></span>查询日期</view>
-					<picker mode="date" start="2019-09" end="2030-09" fields="month" v-model:value="date" @change="bindDateChange">
+					<picker mode="date" start="2019-09" end="2030-09" fields="month" v-model:value="date"
+						@change="bindDateChange">
 						<view class="picker">
 							{{date}}
 						</view>
 					</picker>
 				</view>
-				<view class="qiun-charts">
-					<canvas canvas-id="canvasPie" id="canvasPie" class="charts" @touchstart="touchPie"></canvas>
+				<view>
+					<view class="charts-box">
+						<qiun-data-charts type="pie" :chartData="chartData" background="none" />
+					</view>
 				</view>
-
 			</view>
 
-			<view style="text-align: center;font-size: 40rpx;margin-top: 25rpx;">隐患所在区域统计</view>
+			<view style="text-align: center;margin:20rpx ;font-size: 30rpx;">部门上报隐患统计</view>
 			<view>
-				<view class="qiun-charts">
-					<canvas canvas-id="canvasColumn" id="canvasColumn" class="charts" @touchstart="touchColumn"></canvas>
+				<view class="charts-box">
+					<qiun-data-charts type="column" :chartData="chartData1" background="none" />
 				</view>
 			</view>
-			<view style="text-align: center;font-size: 40rpx;margin-top: 25rpx;">隐患类别统计</view>
-			<view class="qiun-charts">
-				<canvas canvas-id="canvasRing" id="canvasRing" class="charts" @touchstart="touchRing"></canvas>
-			</view>
 
+			<view style="text-align: center;margin:20rpx ;font-size: 30rpx;">区域隐患统计</view>
+			<view>
+				<view style="height: 800rpx;">
+					<qiun-data-charts type="ring" :chartData="chartData2" background="none" />
+				</view>
+			</view>
 
 		</view>
-
-
+		<!-- 重大危险源 -->
+		<view style="margin-top: 40rpx;">
+			<text style="font-size: 40rpx;font-weight: 500;margin: 20rpx;">重大危险源统计</text>
+			<view style="margin-top: 20rpx;">
+				<u-line color="green" border-style="dashed"></u-line>
+			</view>
+			
+			<view style="width: 98%;height: 500rpx;display: flex;justify-content: space-around;flex-wrap: wrap;margin-top: 40rpx;margin-left: 1%;">
+				<view style="width: 43%;height: 200rpx;background-color: #f33f58;border-radius: 20rpx;box-shadow: 6px 6px 5px #868686;">
+					<view style="color: #FFFFFF;text-align: center;padding-top: 50rpx;font-size: 30rpx;">一级重大危险源:2</view>
+					<view style="color: #FFFFFF;text-align: center;padding-top: 20rpx;">液氨罐区、原料及产品罐区</view>
+				</view>
+				<view style="width: 43%;height: 200rpx;background-color: #934ef6;border-radius: 20rpx;box-shadow: 6px 6px 5px #868686;">
+					<view style="color: #FFFFFF;text-align: center;padding-top: 50rpx;font-size: 30rpx;">四级重大危险源:3</view>
+					<view style="color: #FFFFFF;text-align: center;padding-top: 20rpx;">环己醇装置区、中间品罐区、聚甲醛中间罐区</view>
+				</view>
+				<view style="width: 43%;height: 200rpx;background-color: #5376ea;border-radius: 20rpx;box-shadow: 6px 6px 5px #868686;">
+					<view style="color: #FFFFFF;text-align: center;padding-top: 50rpx;font-size: 30rpx;">重点监管危化品:4</view>
+					<view style="color: #FFFFFF;text-align: center;padding-top: 20rpx;">甲醇、纯苯、液氨、氢气</view>
+				</view>
+				<view style="width: 43%;height: 200rpx;background-color: #ffc331;border-radius: 20rpx;box-shadow: 6px 6px 5px #868686;">
+					<view style="color: #FFFFFF;text-align: center;padding-top: 50rpx;font-size: 30rpx;">重点监管工艺:2</view>
+					<view style="color: #FFFFFF;text-align: center;padding-top: 20rpx;">氧化装置和加氢装置</view>
+				</view>
+			</view>
+		</view>
+		<!-- 八大作业证统计 -->
+		<view style="margin-top: 40rpx;">
+			<text style="font-size: 40rpx;font-weight: 500;margin: 20rpx;">八大作业证统计:</text>
+			<view style="margin-top: 20rpx;">
+				<u-line color="green" border-style="dashed"></u-line>
+			</view>
+			<view>
+				<qiun-data-charts type="rose" :chartData="chartData3" background="none" />
+				
+			</view>
+			</view>
 	</view>
+
 </template>
 
 <script>
-	import uCharts from '@/js_sdk/u-charts/u-charts/u-charts.js';
-	var _self;
-	var canvaPie = null;
-	var canvaRing=null;
-	var canvaColumn = null;
 	export default {
-
 		data() {
 			return {
+				day: '',
 				date: '2020-12',
-				cWidth: '',
-				cHeight: '',
-				pixelRatio: 1,
-				serverData: '',
-				canvaColumn: {}
+				chartData: {
+					"series": [{
+						"data": [{
+								"name": "整改中",
+								"value": ''
+							},
+							{
+								"name": "按期整改",
+								"value": ''
+							},
+							{
+								"name": "超期未整改",
+								"value": ''
+							},
 
+						]
+					}]
+				},
+				chartData1: {
+					"categories": [
+					
+					],
+					"series": [{
+							"name": "隐患上报数",
+							"data": [
+							
+							]
+						}
 
+					]
+				},
+				chartData2: {
+					"series": [{
+						"data": [
+						]
+					}]
+				},
+				chartData3:{
+					 "series": [
+					        {
+					            "data": [
+					                {
+					                    "name": "动火作业证",
+					                    "value": 0
+					                },
+					                {
+					                    "name": "动土作业证",
+					                    "value": 0
+					                },
+					                {
+					                    "name": "断路作业证",
+					                    "value": 0
+					                },
+					                {
+					                    "name": "盲板抽堵作业证",
+					                    "value": 0
+					                },
+					                {
+					                    "name": "临时用电作业证",
+					                    "value": 0
+					                },
+									{
+									    "name": "高处作业证",
+									    "value": 0
+									},
+									{
+									    "name": "吊装作业证",
+									    "value": 0
+									},
+									{
+									    "name": "受限空间作业证",
+									    "value": 0
+									},
+					            ]
+					        }
+					    ]
+				}
 			}
 		},
 		methods: {
 			bindDateChange: function(e) {
 				this.date = e.target.value
+			
 				this.getServerData()
 			},
-			async getHuan() {
-
-
+			async getServerData(){
 				const res = await this.$myRequest({
-					url: '/api/stac/getDangerByClass',
-
-					method: 'POST'
+					method: 'POST',
+					url: 'api/other/getYhTj',
+					data:{date:this.date}
 				})
-				let Ring={series:[]};
-				Ring.series=res.data.data
-				
-				_self.showRing("canvasRing",Ring);
-
-			},
-			showRing(canvasId, chartData) {
-				canvaRing = new uCharts({
-					$this: _self,
-					canvasId: canvasId,
-					type: 'ring',
-					fontSize: 11,
-					legend: true,
-					// title: {
-					// 	name: '70%',
-					// 	color: '#7cb5ec',
-					// 	fontSize: 25 * _self.pixelRatio,
-					// 	offsetY: -20 * _self.pixelRatio,
-					// },
-					// subtitle: {
-					// 	name: '收益率',
-					// 	color: '#666666',
-					// 	fontSize: 15 * _self.pixelRatio,
-					// 	offsetY: 30 * _self.pixelRatio,
-					// },
-					extra: {
-						pie: {
-							offsetAngle: -45,
-							ringWidth: 40 * _self.pixelRatio,
-							labelWidth: 15
-						}
-					},
-					background: '#FFFFFF',
-					pixelRatio: _self.pixelRatio,
-					series: chartData.series,
-					animation: true,
-					width: _self.cWidth * _self.pixelRatio,
-					height: _self.cHeight * _self.pixelRatio,
-					disablePieStroke: true,
-					dataLabel: true,
-				});
-			},
-			touchRing(e) {
-				canvaRing.showToolTip(e, {
-					format: function(item) {
-						return item.name + ':' + item.data
-					}
-				});
-			},
-			async getServerData() {
-
-				const res = await this.$myRequest({
-					url: '/api/stac/getDangerStacByComrate',
-					data: {
-						year: this.date
-					},
-					method: 'POST'
-				})
-				var data = res.data.data
-				var ser = [{
-						"name": "整改中",
-						"data": data.completeingCount
-					},
-					{
-						"name": "按期整改",
-						"data": data.completeCount
-					}, {
-						"name": "超期未整改",
-						"data": data.noCompleteCount
-					}
-				]
-
-				let Pie = {
-					series: []
-				};
-				Pie.series = ser
-
-				_self.showPie("canvasPie", Pie)
-				// uni.request({
-				// 	url: 'https://www.ucharts.cn/data.json',
-				// 	data: {},
-				// 	success: function(res) {
-
-				// 		console.log(res.data.data)
-				// 		let Pie = {
-				// 			series: []
-				// 		};
-				// 		//这里我后台返回的是数组，所以用等于，如果您后台返回的是单条数据，需要push进去
-				// 		Pie.series = res.data.data.Pie.series;
-				// 		_self.showPie("canvasPie", Pie);
-				// 	},
-				// 	fail: () => {
-				// 		_self.tips = "网络错误，小程序端请检查合法域名";
-				// 	},
-				// });
-			},
-			showPie(canvasId, chartData) {
-				canvaPie = new uCharts({
-					$this: _self,
-					canvasId: canvasId,
-					type: 'pie',
-					fontSize: 11,
-					legend: {
-						show: true
-					},
-					background: '#FFFFFF',
-					pixelRatio: _self.pixelRatio,
-					series: chartData.series,
-					animation: true,
-					width: _self.cWidth * _self.pixelRatio,
-					height: _self.cHeight * _self.pixelRatio,
-					dataLabel: true,
-					extra: {
-						pie: {
-							lableWidth: 15
-						}
-					},
-				});
-			},
-			touchPie(e) {
-				canvaPie.showToolTip(e, {
-					format: function(item) {
-						return item.name + ':' + item.data
-					}
-				});
-			},
-			async getZhu() {
-				const res = await this.$myRequest({
-					url: '/api/stac/getDangerByArea',
-
-					method: 'POST'
-				})
-				var data = res.data.data
-				let Column = data
-				_self.showColumn("canvasColumn", Column);
-			},
-			showColumn(canvasId, chartData) {
-				canvaColumn = new uCharts({
-					$this: _self,
-					canvasId: canvasId,
-					type: 'column',
-					legend: {
-						show: true
-					},
-					fontSize: 5,
-					background: '#FFFFFF',
-					pixelRatio: _self.pixelRatio,
-					animation: true,
-					categories: chartData.categories,
-					series: chartData.series,
-					xAxis: {
-						disableGrid: true,
-					},
-					yAxis: {
-						//disabled:true
-					},
-					dataLabel: true,
-					width: _self.cWidth * _self.pixelRatio,
-					height: _self.cHeight * _self.pixelRatio,
-					extra: {
-						column: {
-							type: 'group',
-							width: _self.cWidth * _self.pixelRatio * 0.45 / chartData.categories.length
-						}
-					}
-				});
-
-			},
-			touchColumn(e) {
-				canvaColumn.showToolTip(e, {
-					format: function(item, category) {
-						if (typeof item.data === 'object') {
-							return category + ' ' + item.name + ':' + item.data.value
-						} else {
-							return category + ' ' + item.name + ':' + item.data
-						}
-					}
-				});
-			},
-
-
+			
+				//整改中
+				this.chartData.series[0].data[0].value = res.data.data.zgzCount
+				//按期
+				this.chartData.series[0].data[1].value = res.data.data.wcCount
+				//未整改
+				this.chartData.series[0].data[2].value = res.data.data.cqCount
+			}
 		},
-		onLoad() {
-			_self = this;
-			this.cWidth = uni.upx2px(750);
-			this.cHeight = uni.upx2px(500);
-			this.getServerData();
-			this.getZhu()
-			this.getHuan()
+		async onShow() {
+			//获取当前月
+			var myDate = new Date();
+			var tYear = myDate.getFullYear();
+			var tMonth = myDate.getMonth();
+			
+			var m = tMonth + 1;
+			if (m.toString().length == 1) {
+			m = "0" + m;
+			}
+			this.date = tYear +'-'+ m
+			const res = await this.$myRequest({
+				method: 'POST',
+				url: 'api/other/getStac',
+			})
+			//赋值天数
+			this.day = res.data.data.days
+			//整改中
+			this.chartData.series[0].data[0].value = res.data.data.zgzCount
+			//按期
+			this.chartData.series[0].data[1].value = res.data.data.wcCount
+			//未整改
+			this.chartData.series[0].data[2].value = res.data.data.cqCount
+			
+			//部门上报隐患
+			var bm = []
+			var num = []
+			for(var i = 0;i<res.data.data.bmYhs.length;i++){
+				bm.push(res.data.data.bmYhs[i].bmName)
+				num.push(res.data.data.bmYhs[i].num)
+			}
+			this.chartData1.categories = bm
+			this.chartData1.series[0].data = num
+			//区域隐患
+			this.chartData2.series[0].data = res.data.data.areaYhs
+			//八大作业证
+			
+			this.chartData3.series[0].data[0].value = res.data.data.dhCount
+			this.chartData3.series[0].data[1].value = res.data.data.dtCount
+			this.chartData3.series[0].data[2].value = res.data.data.dlCount
+			this.chartData3.series[0].data[3].value = res.data.data.mbCount
+			this.chartData3.series[0].data[4].value = res.data.data.lsCount 
+			this.chartData3.series[0].data[5].value = res.data.data.gcCount
+			this.chartData3.series[0].data[6].value = res.data.data.dzCount
+			this.chartData3.series[0].data[7].value = res.data.data.sxCount
+		
 		}
 	}
 </script>
 
 <style>
-	page {
-		background: #F2F2F2;
-		width: 750upx;
-		overflow-x: hidden;
-	}
 
-	.qiun-padding {
-		padding: 2%;
-		width: 96%;
-	}
-
-	.qiun-wrap {
-		display: flex;
-		flex-wrap: wrap;
-	}
-
-	.qiun-rows {
-		display: flex;
-		flex-direction: row !important;
-	}
-
-	.qiun-columns {
-		display: flex;
-		flex-direction: column !important;
-	}
-
-	.qiun-common-mt {
-		margin-top: 10upx;
-	}
-
-	.qiun-bg-white {
-		background: #FFFFFF;
-	}
-
-	.qiun-title-bar {
-		width: 96%;
-		padding: 10upx 2%;
-		flex-wrap: nowrap;
-	}
-
-	.qiun-title-dot-light {
-		border-left: 10upx solid #0ea391;
-		padding-left: 10upx;
-		font-size: 32upx;
-		color: #000000
-	}
-
-	.qiun-charts {
-		width: 750upx;
-		height: 500upx;
-		background-color: #FFFFFF;
-	}
-
-	.charts {
-		width: 750upx;
-		height: 500upx;
-		background-color: #FFFFFF;
-	}
 </style>
